533301f7121cc7360ad05d5f9acc4a8c87c301f3
ruggerifrancesco <lavoro.francescoruggeri@gmail.com>
2023-06-05 11:38:55 +0200

    created array 4 el, print 4 li item tags

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 import { createApp } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js'
 
+// Genero due cicli for con vue, uno che cicla in un array di quattro 
+// elementi e li stampa su schermo, l'altro che stampa su schermo i primi 10 numeri.
+
 createApp({
   data() {
     return {
-      message: 'Hello Vue!'
+      message: 'Hello Vue!',
+      array: [
+        'ciao',
+        345,
+        "Python",
+        2 + 1
+      ]
     }
   }
 }).mount('#app')
